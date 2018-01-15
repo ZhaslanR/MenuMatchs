1312ecf08a774636dff73b198e1a58864c04a27f
ZhaslanR <zhaslan_12@bk.ru>
2018-01-15 17:49:23 +0600

    Добавьте файлы проекта.

---
+++ LoginAndRegister/MainMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LoginAndRegister
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void signInButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+            this.Close();
+        }
+
+        private void signUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow window = new SecondWindow();
+            window.Show();
+            this.Close();
+        }
+    }
+}
